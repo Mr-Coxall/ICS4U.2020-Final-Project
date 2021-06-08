88dbffbbbc124f4f177a6a937e5ac819e0880a04
cameron-teed <54957609+cameron-teed@users.noreply.github.com>
2021-06-08 15:26:14 -0400

    Fixed style for Assets.java

--- CameronTeed/Breakfast/src/com/breakfast/main/Assets.java
+++ CameronTeed/Breakfast/src/com/breakfast/main/Assets.java
@@ -9,18 +9,105 @@ package com.breakfast.main;
  */
 import java.awt.image.BufferedImage;
 
-public class Assets {
-  public static BufferedImage spriteSheet;
-  public static BufferedImage spatula, spoon, egg, bacon;
+final class Assets {
+  // This is the Asset Class.
 
-  public static void init() {
+  /**
+   * Initializing variable.
+   */
+  private static BufferedImage spatula;
+  /**
+   * Initializing variable.
+   */
+  private static BufferedImage spoon;
+  /**
+   * Initializing variable.
+   */
+  private static BufferedImage egg;
+  /**
+   * Initializing variable.
+   */
+  private static BufferedImage bacon;
+  /**
+   * Initializing variable.
+   */
+  private final int imageW = 70;
+  /**
+   * Initializing variable.
+   */
+  private final int imageH = 70;
+  /**
+   * Initializing variable.
+   */
+  private final int col = 3;
+  /**
+   * Initializing variable.
+   */
+  private final int row1 = 2;
+  /**
+   * Initializing variable.
+   */
+  private final int row2 = 1;
+  /**
+   * Initializing variable.
+   */
+  private final int row3 = 3;
+  /**
+   * Initializing variable.
+   */
+  private final int imageBacon = 73;
+  /**
+   * Initializing variable.
+   */
+  private final int imageEgg = 75;
+
+  public Assets(){
+      
+  }
+
+  /**
+   * This method initializes the sprites.
+   */
+  public void init() {
     BufferedImageLoader loader = new BufferedImageLoader();
     SpriteLoader ss = new SpriteLoader(loader.loadImage("/pancakes.png"));
     SpriteLoader bs = new SpriteLoader(loader.loadImage("/eggs.png"));
 
-    spatula = ss.grabImage(3, 2, 70, 70);
-    spoon = ss.grabImage(3, 1, 70, 70);
-    bacon = ss.grabImage(3, 3, 73, 73);
-    egg = bs.grabImage(3, 2, 70, 75);
+    spatula = ss.grabImage(col, row1, imageW, imageH);
+    spoon = ss.grabImage(col, row2, imageW, imageH);
+    bacon = ss.grabImage(col, row3, imageBacon, imageBacon);
+    egg = bs.grabImage(col, row1, imageW, imageEgg);
+  }
+  
+  /**
+   * Getter for a sprite.
+   * @return
+   */
+  public BufferedImage getImage() {
+      return spatula;
+  }
+
+  /**
+   * Getter for a sprite.
+   * @return
+   */
+  public BufferedImage getImage2() {
+      return spoon;
+  }
+
+  /**
+   * Getter for a sprite.
+   * @return
+   */
+  public BufferedImage getImage3() {
+      return bacon;
+  }
+
+  /**
+   * Getter for a sprite.
+   * @return
+   */
+  public BufferedImage getImage4() {
+      return egg;
   }
 }
