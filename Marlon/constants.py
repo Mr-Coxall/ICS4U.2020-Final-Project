a78777f5fdd3619a306975312779311b6b952902
Marlon Poddalgoda <74619000+Marlon-Poddalgoda@users.noreply.github.com>
2021-06-08 19:22:04 -0400

    Added constants.py

---
+++ Marlon/constants.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Created by Marlon Poddalgoda
+# Created on June 2021
+# This file holds the constants for the platforming game.
+
+# CONSTANTS
+SCREEN_WIDTH = 1500
+SCREEN_HEIGHT = 400
+SCREEN_TITLE = "Platformer"
