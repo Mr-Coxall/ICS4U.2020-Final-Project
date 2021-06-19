58be554e4ae3c063476765d14dda0476bb955fec
Sean-McLeod <60712857+Sean-McLeod@users.noreply.github.com>
2021-06-18 21:29:57 -0400

    Update check_prisoner_event.py

--- SeanMcLeod/Code/check_prisoner_event.py
+++ SeanMcLeod/Code/check_prisoner_event.py
@@ -1,48 +1,40 @@
+#!/usr/bin/env python3
+
+# Created by Sean McLeod
+# Created on June 2021
+# This is the check prisoner event class
+
 import sys
 
 import pygame
 
 
 class CheckPrisonerEvents:
-    def __init__(self):
-        self.key_left = False
-        self.key_right = False
-        self.key_up = False
-        self.key_down = False
-        self.key_is_up = False
-        self.key_is_down = False
-
-    def check_events(self):
+    @staticmethod
+    def check_events():
         # every loop, set these values to False in order to refresh them
-        self.key_left = False
-        self.key_right = False
-        self.key_up = False
-        self.key_down = False
-        self.key_is_up = False
-        self.key_is_down = False
+        key_left = False
+        key_right = False
+        key_up = False
+        key_down = False
+        key_is_up = False
+        key_is_down = False
         # for events, check what key is pressed and return the values
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
-                self.key_is_down = True
+                key_is_down = True
                 if event.key == pygame.K_LEFT:
-                    self.key_left = True
+                    key_left = True
                 if event.key == pygame.K_RIGHT:
-                    self.key_right = True
+                    key_right = True
                 if event.key == pygame.K_UP:
-                    self.key_up = True
+                    key_up = True
                 if event.key == pygame.K_DOWN:
-                    self.key_down = True
+                    key_down = True
 
             if event.type == pygame.KEYUP:
-                self.key_is_up = True
+                key_is_up = True
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        return (
-            self.key_is_down,
-            self.key_left,
-            self.key_right,
-            self.key_up,
-            self.key_down,
-            self.key_is_up,
-        )
+        return key_is_down, key_left, key_right, key_up, key_down, key_is_up
